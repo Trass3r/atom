428cc6af0ac389a2a51b08b909022dc82a9646bb
Andreas Hollandt <trass3r@gmail.com>
2018-01-18 21:26:11 +0100

    port checkerboard example to Generator concept

--- examples/checkerboard.cpp
+++ examples/checkerboard.cpp
@@ -1,13 +1,39 @@
 // Copyright 2015 Adobe Systems Incorporated
 // All Rights Reserved.
 
-Var x, y, c;
+#include <Halide.h>
+#include <string>
 
-Param<int> size("size", 32, 4, 256);
-Param<int> shade_1("shade 1", 192, 0, 255);
-Param<int> shade_2("shade 2", 64, 0, 255);
+using namespace Halide;
 
-result(x, y, c) = cast<uint8_t>(select(
-    (floor (x / size) % 2) != (floor(y / size) % 2), shade_1, shade_2));
+struct MyFirstGenerator : public Halide::Generator<MyFirstGenerator>
+{
+	Var x{"x"}, y{"y"}, c{"c"};
 
-result.parallel(y).vectorize(x, 8);
+	Input<int> size{"size", 32, 4, 256};
+	Input<int> shade_1{"shade1", 192, 0, 255};
+	Input<int> shade_2{"shade2", 64, 0, 255};
+
+	Output<Buffer<uint8_t>> result{"result", 3};
+
+	void generate()
+	{
+		result(x, y, c) = cast<uint8_t>(select((floor (x / size) % 2) != (floor(y / size) % 2), shade_1, shade_2));
+	}
+
+	void schedule()
+	{
+		if (auto_schedule)
+		{
+			result.estimate(c, 0, 3)
+				.estimate(x, 0, 512)
+				.estimate(y, 0, 512);
+		}
+		else
+		{
+			result.parallel(y).vectorize(x, 8);
+		}
+	}
+};
+
+HALIDE_REGISTER_GENERATOR(MyFirstGenerator, generator)
