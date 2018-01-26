e1050a1323f6d7fbaa2733fcdaaf38fef4d4c356
Andreas Hollandt <trass3r@gmail.com>
2018-01-27 00:14:39 +0100

    first though wrong color demosaic

--- examples/demosaic.cpp
+++ examples/demosaic.cpp
@@ -8,6 +8,13 @@ using namespace Halide::ConciseCasts;
 // Shared variables
 Var x("x"), y("y"), c("c"), yi("yi"), yo("yo"), yii("yii"), xi("xi");
 
+Var x_i("x_i");
+Var x_i_vi("x_i_vi");
+Var x_i_vo("x_i_vo");
+Var x_o("x_o");
+Var x_vi("x_vi");
+Var x_vo("x_vo");
+
 // Average two positive values rounding up
 Expr avg(Expr a, Expr b)
 {
@@ -39,13 +46,13 @@ struct Demosaic final : public Halide::Generator<Demosaic>
     Input<Func> input{"input", Int(16), 3}; // in RGGB bands
     Output<Func> output{"output", Int(16), 3};
 
+    Input<bool> hmm{"hmm", false};
+
     Func _(R), _(Gr), _(Gb), _(B);
+    Func _(outGb), _(outGr);
 
     void generate()
     {
-        // pattern:
-        // G B
-        // R G
         R (x, y) = input(x + 1, y + 1, 0);
         Gb(x, y) = input(x + 1, y + 1, 1);
         Gr(x, y) = input(x + 1, y + 1, 2);
@@ -53,15 +60,20 @@ struct Demosaic final : public Halide::Generator<Demosaic>
 
         // TODO: BoundaryConditions::repeat_edge
 
+        // pattern:
+        // G B
+        // R G
+
+        Func _(Ghb), _(Ghr), _(Gvb), _(Gvr);
         // even rows = blue
-        auto Ghb = (Gb(x, y) + Gb(x+1, y)) / 2  + (2 * B(x, y) - B(x-1, y) - B(x+1, y)) / 4;
+        Ghb(x, y) = (Gb(x, y) + Gb(x+1, y)) / 2  + (2 * B(x, y) - B(x-1, y) - B(x+1, y)) / 4;
         // odd rows = red
-        auto Ghr = (Gr(x-1, y) + Gr(x, y)) / 2  + (2 * R(x, y) - R(x-1, y) - R(x+1, y)) / 4;
+        Ghr(x, y) = (Gr(x-1, y) + Gr(x, y)) / 2  + (2 * R(x, y) - R(x-1, y) - R(x+1, y)) / 4;
 
         // even cols = red
-        auto Gvr = (Gb(x, y) + Gb(x, y+1)) / 2  + (2 * R(x, y) - R(x, y-1) - R(x, y+1)) / 4;
+        Gvr(x, y) = (Gb(x, y) + Gb(x, y+1)) / 2  + (2 * R(x, y) - R(x, y-1) - R(x, y+1)) / 4;
         // odd cols = blue
-        auto Gvb = (Gr(x, y-1) + Gr(x, y)) / 2  + (2 * B(x, y) - B(x, y-1) - B(x, y+1)) / 4;
+        Gvb(x, y) = (Gr(x, y-1) + Gr(x, y)) / 2  + (2 * B(x, y) - B(x, y-1) - B(x, y+1)) / 4;
 
 		// chrominances of reconstructed images
 		Func _(Chb), _(Chr), _(Cvb), _(Cvr);
@@ -69,6 +81,9 @@ struct Demosaic final : public Halide::Generator<Demosaic>
 		Chr(x, y) = Ghr(x, y) - R(x, y);
 		Cvb(x, y) = Gvb(x, y) - B(x, y);
 		Cvr(x, y) = Gvr(x, y) - R(x, y);
+
+        //output(x, y, c) = select(hmm, Chb(x/2, y/2), Cvb(x/2, y/2));
+        //return;
 
 		// gradients of chrominances
 		Func _(Dhb), _(Dhr), _(Dvb), _(Dvr);
@@ -84,16 +99,69 @@ struct Demosaic final : public Halide::Generator<Demosaic>
 		deltaVb(x, y) = C * Dvr(x, y) + C * Dvr(x-1, y) + Dvr(x-1, y-1) + Dvr(x-1, y+1) + Dvr(x, y-1) + Dvr(x, y+1) + Dvb(x-1, y-1) + Dvb(x-1, y);
 		deltaVr(x, y) = C * Dvb(x, y) + C * Dvb(x-1, y) + Dvb(x-1, y-1) + Dvb(x-1, y+1) + Dvb(x, y-1) + Dvb(x, y+1) + Dvr(x, y) + Dvr(x, y+1);
 
-		Func _(outG);
-		outGb(x, y) = select(deltaVb < deltaHb, Gvb, Ghb);
-		outGr(x, y) = select(deltaVr < deltaHr, Gvr, Ghr);
+        // pattern:
+        // G B G B G B G B
+        // R G R G R G R G
+        // G B G[B]G B G B
+        // R G R G R G R G
+        // G B G B G B G B
+        Expr Eb = deltaVb(x, y) < deltaHb(x, y);
+        Expr Er = deltaVr(x, y) < deltaHr(x, y);
+        outGb(x, y) = select(Eb, Gvb(x, y), Ghb(x, y));
+		outGr(x, y) = select(Er, Gvr(x, y), Ghr(x, y));
 
-        Func _(Gh), _(Gv);
+        Func _(outG);
+        //Func _(Gh), _(Gv);
 		// Gh(x, y) = select(y % 2 == 0, Ghb, Ghr);
 		// Gv(x, y) = select(x % 2 == 0, Gvr, Gvb);
-        outG(x, y) = select(y % 2 == 0, outGb(x, y), outGr(x, y)); // TODO: / 2 ?
+        outG(x, y) = select(y % 2 == 0,
+            select(x % 2 == 0, Gb(x/2, y/2), outGb(x/2, y/2)),
+            select(x % 2 == 0, outGr(x/2, y/2), Gr(x/2, y/2))
+        );
 
-        output(x, y, c) = outG(x, y); // Gh(max(x/2-2, 0), max(y/2-2, 0));
+        Func _(Rgr), _(Rgb), _(Bgr), _(Bgb);
+        // G B G B
+        // R G R G
+        // G[B]G B
+        // R G R G
+
+        // green pixels: red through bilinear interpolation of R-G
+        Bgb(x, y) = Gb(x, y) + ( B(x-1, y)-Gb(x-1, y) + B(x, y)-Gb(x-1, y) )/2;
+        Rgb(x, y) = Gb(x, y) + ( R(x-1, y)-Gr(x-1, y) + R(x, y)-Gr(x-1, y) )/2;
+        Bgr(x, y) = Gb(x, y) + ( B(x, y)-Gb(x, y) + B(x+1, y)-Gb(x+1, y) )/2;
+        Rgr(x, y) = Gr(x, y) + ( R(x, y)-Gr(x, y) + R(x+1, y)-Gr(x+1, y) )/2;
+
+        // reconstruction of the red and blue values in the blue and red pixels,
+        // respectively.
+
+        // R-B = R-(Gb-1) + R-(Gb+1)
+        Func _(Rb), _(Br);
+        Rb(x, y) = select(Er,
+            B(x, y) + (Rgb(x, y) - Bgb(x, y) + Rgb(x + 1, y) - Bgb(x + 1, y)) / 2 // (B(x-1, y) B(x+1, y)) / 2
+            ,
+            B(x, y) + (Rgb(x, y) - Bgb(x, y) + Rgb(x, y + 1) - Bgb(x, y + 1)) / 2 // (B(x-1, y) B(x+1, y)) / 2
+        );
+        Br(x, y) = select(Eb,
+            R(x, y) + (Bgr(x, y) - Rgr(x, y) + Bgr(x + 1, y) - Rgr(x + 1, y)) / 2 // (B(x-1, y) B(x+1, y)) / 2
+            ,
+            R(x, y) + (Bgr(x, y) - Rgr(x, y) + Bgr(x, y + 1) - Rgr(x, y + 1)) / 2 // (B(x-1, y) B(x+1, y)) / 2
+        );
+
+        //output(x, y, c) = outG(x, y);
+        // G B G B
+        // R G R G
+        output(x, y, c) = select(c == 1,
+            outG(x, y),
+            c == 0,
+            select(y % 2 == 0,
+                select(x % 2 == 0, Bgb(x/2, y/2), B(x/2, y/2)),
+                select(x % 2 == 0, Br(x/2, y/2), Bgr(x/2, y/2))
+            ),
+            select(y % 2 == 0,
+                select(x % 2 == 0, Rgb(x/2, y/2), Rb(x/2, y/2)),
+                select(x % 2 == 0, R(x/2, y/2), Rgr(x/2, y/2))
+            )
+        );
     }
 
     void schedule()
@@ -105,6 +173,8 @@ struct Demosaic final : public Halide::Generator<Demosaic>
         Gr.compute_root();
         Gb.compute_root();
         B.compute_root();
+        outGb.compute_root();
+        outGr.compute_root();
     }
 };
 
@@ -113,13 +183,15 @@ struct CameraPipe final : public Halide::Generator<CameraPipe>
     Input<Buffer<uint8_t>> input{"input", 3};
     Output<Buffer<uint8_t>> processed{"processed", 3};
 
-    GeneratorInput<bool> showRaw{"showRaw", false};
+    Input<bool> showRaw{"showRaw", false};
+    Input<bool> hmm{"hmm", false};
 
     void generate();
     void schedule();
 
     Func _(deinterleaved);
     Func _(base);
+    Func _(raw);
     std::unique_ptr<Demosaic> demosaiced;
 private:
     Func deinterleave(Func raw);
@@ -138,8 +210,8 @@ Func CameraPipe::deinterleave(Func raw)
 
 void CameraPipe::generate()
 {
-    //base(x, y, c) = cast<uint16_t>(BoundaryConditions::mirror_image(input)(x, y, c));
-    base(x, y, c) = cast<int16_t>(input(x, y, c));
+    base(x, y, c) = cast<int16_t>(BoundaryConditions::mirror_image(input)(x, y, c));
+    //base(x, y, c) = cast<int16_t>(input(x, y, c));
 
     // turn RGB into bayer AND deinterleave
     deinterleaved(x, y, c) = select(c == 1, base(2*x, 2*y, 1),     // green0
@@ -147,12 +219,11 @@ void CameraPipe::generate()
                                     c == 0, base(2*x, 2*y+1, 0),   // red
                                             base(2*x+1, 2*y, 2));  // blue
 
-    Func _(raw);
     raw(x, y) = base(x, y, select((x%2) == (y%2), 1,
                         (x%2)==0 && (y%2) == 1, 0,
                         2));
     demosaiced = create<Demosaic>();
-    demosaiced->apply(deinterleaved);
+    demosaiced->apply(deinterleaved, hmm);
 
     Expr forDisplay = select(showRaw, raw(x, y), demosaiced->output(x, y, c));
     processed(x, y, c) = cast<uint8_t>(clamp(forDisplay, 0, 255));
@@ -175,19 +246,24 @@ void CameraPipe::schedule()
     {
         base.compute_root();
 
+        raw
+            .compute_at(processed, x_o)
+            .split(x, x_vo, x_vi, 16)
+            .vectorize(x_vi);
+
         Expr out_width = processed.width();
         Expr out_height = processed.height();
         int vec = get_target().natural_vector_size(UInt(16));
         Expr strip_size = 32;
         strip_size = (strip_size / 2) * 2;
-        processed.compute_root()
-            .reorder(c, x, y)
-            //.split(y, yi, yii, 2, TailStrategy::RoundUp)
-            //.split(yi, yo, yi, strip_size / 2)
-            .vectorize(x, /* 2* */vec, TailStrategy::RoundUp)
-            .unroll(c)
-            //.parallel(yo)
-            ;
+        processed
+            .compute_root()
+            .split(x, x_o, x_i, 64)
+            .reorder(x_i, y, c, x_o)
+            .split(x_i, x_i_vo, x_i_vi, 32)
+            .vectorize(x_i_vi)
+            //.parallel(x_o)
+        ;
 
 // We can generate slightly better code if we know the splits divide the extent.
         processed
